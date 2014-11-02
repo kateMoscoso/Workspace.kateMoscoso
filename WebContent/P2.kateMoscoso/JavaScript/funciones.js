b25abd13183db02a4312391ef42e3a0a7b605352
kateMoscoso <katiemscs@gmail.com>
2014-11-03 00:40:33 +0100

    03/11

--- WebContent/P2.kateMoscoso/JavaScript/funciones.js
+++ WebContent/P2.kateMoscoso/JavaScript/funciones.js
@@ -1,6 +1,17 @@
 
 
-function Calcula(){
+function fechaHoy(){
+	
+	var fecha = new Date();
+	var dia_i = fecha.getDate();
+	var mes_i = fecha.getMonth()+1;
+	var anio_i = fecha.getFullYear();
+	fechaInicio = document.getElementById("fechas");
+ 
+	//textFechaInicio = document.createTextNode("Fecha de Inicio: " + dia_i+"/"+mes_i+"/"+anio_i);
+		fechaInicio.childNodes[2].nodeValue=dia_i+"/"+mes_i+"/"+anio_i;
+}
+function CalculaFecha1(){
 var fecha = new Date();
 var ms_dias = 0;
 var ms_hastahoy = fecha.getTime();
@@ -16,28 +27,26 @@ if (document.getElementById("cientoochenta").checked) {
 
 }
 
-var dia_i = fecha.getDate();
-var mes_i = fecha.getMonth()+1;
-var anio_i = fecha.getFullYear();
-document.getElementById('inicio').value = dia_i+"/"+mes_i+"/"+anio_i;
 var ms_futuro = ms_hastahoy + ms_dias;
 var fecha_futura = new Date();
 fecha_futura.setTime(ms_futuro);
 var dia_f = fecha_futura.getDate();
 var mes_f = fecha_futura.getMonth()+1;
 var anio_f = fecha_futura.getFullYear();
-document.getElementById('fin').value = dia_f+"/"+mes_f+"/"+anio_f;
-
+fechaInicio = document.getElementById("fechas");
+//textFechaFin = document.createTextNode("Fecha de Fin: " + dia_f+"/"+mes_f+"/"+anio_f);
+fechaInicio.childNodes[6].nodeValue =dia_f+"/"+mes_f+"/"+anio_f;
 }
 
-function CalculaFecha(){
+function CalculaFecha2(){
 	document.getElementById('fin').value = document.getElementById("fechafin").value;
 }
-
+function obtenerCalendario(){
+	
+}
 function Add(){
 	if (numFilas < 8) {
   		var tabla = document.getElementById("bodyTablas");
-  		alert("Hola");
   		newNode=document.getElementById("elemento").cloneNode(true);
   		tabla.appendChild(newNode);
   		numFilas++;
@@ -51,9 +60,29 @@ function Elimina(){
 	}
 }
 
-function Calcular(){
+function Calcular1(){
 var subtotal=0, total =0,iva=0;
-tabla = document.getElementById("bodyTablas");
+var tabla = document.getElementById("bodyTablas");
+  for (var i=4; i < tabla.childNodes.length; i++){
+  	if (tabla.childNodes[i].nodeType == 1){
+  		var resultado=parseInt(tabla.childNodes[i].childNodes[3].firstChild.value)*parseInt(tabla.childNodes[i].childNodes[5].firstChild.value);
+		tabla.childNodes[i].childNodes[7].firstChild.value=resultado;
+		if(!isNaN(resultado)){
+		subtotal+=parseInt(resultado);
+		}
+  	}
+  }
+
+  document.getElementById("subtotal").childNodes[0].nodeValue=subtotal;
+  iva=subtotal*0.21;
+  document.getElementById("iva").childNodes[0].nodeValue=iva;
+  total=subtotal+iva;
+  document.getElementById("total").childNodes[0].nodeValue=total;
+ CalculaFecha1();
+}
+
+function Calcular2(){
+var subtotal=0, total =0,iva=0;
 
   for (var i=2; i < tabla.childNodes.length; i++){
   	if (tabla.childNodes[i].nodeType == 1){
@@ -64,10 +93,10 @@ tabla = document.getElementById("bodyTablas");
 		}
   	}
   }
-  document.getElementById("subtotal").value=subtotal;
+  document.getElementById("subtotal").childNodes[0].nodeValue=subtotal;
   iva=subtotal*0.21;
-  document.getElementById("iva").value=iva;
+  document.getElementById("iva").childNodes[0].nodeValue=iva;
   total=subtotal+iva;
-  document.getElementById("total").value=total;
- CalculaFecha();
+  document.getElementById("total").childNodes[0].nodeValue=total;
+ CalculaFecha2();
 }
